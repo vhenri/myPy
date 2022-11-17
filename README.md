a83d4de6ae9c67c10c7bf271c045006f8b0d0723
vhenri <victoriabhenri@gmail.com>
2022-11-17 16:17:39 -0500

    - themeing & created welcome fragment

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # myPy
 MyPy is an app that lets you write &amp; run snippets of python code from you Android device!
+
+## Attributions
+- python by Oskar G. from [Noun Project](https://thenounproject.com/icon/python-5241764/)
+- Font - UAV OSD Sans by [Nicholas Kruse](https://nicholaskruse.com/)
--- app/src/main/java/com/vhenri/mypy/ui/main/WelcomeFragment.kt
+++ app/src/main/java/com/vhenri/mypy/ui/main/WelcomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.vhenri.mypy.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
@@ -17,5 +18,16 @@ class WelcomeFragment : Fragment() {
     ): View {
         binding = FragmentWelcomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initObservers()
+    }
+
+    fun initObservers(){
+        binding.startButton.setOnClickListener {
+            findNavController().navigate(WelcomeFragmentDirections.welcomeFragmentToExecFragment())
+        }
     }
 }
